9090dd59b08a8fc21512dc49bad9dcd340b84c2c
Temitope Emokpae <temokpae3@gmail.com>
2025-11-15 17:12:03 -0500

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import ghPages from 'vite-plugin-gh-pages';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), ghPages()],
-  base: '/',
+  base: '/UIUX-Portfolio/',
 })
